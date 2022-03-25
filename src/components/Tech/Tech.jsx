d39a19786348b0f6e5ad36a6cb478f20bd5950a3
daduckster <vlasak.o@yandex.ru>
2022-03-25 16:56:38 +0100

    basic styling for tech section

--- src/components/Tech/Tech.jsx
+++ src/components/Tech/Tech.jsx
@@ -1,10 +1,65 @@
 import React from 'react'
+import {AiFillForward, AiFillGithub} from 'react-icons/ai'
+import {SiCss3, SiSass, SiJavascript, SiHtml5, SiAdobeillustrator} from 'react-icons/si'
+import {FaReact, FaGitAlt} from 'react-icons/fa'
+import {CgFigma} from 'react-icons/cg'
 import styles from './Tech.module.scss';
 
 function Tech() {
   return (
-    <div>
+    <div className={styles.techContainer}>
+      <div className={styles.headerContainer}>
+        <h2>tech</h2>
+        <AiFillForward className={styles.forwardIcon} />
+      </div>
 
+      <div className={styles.iconsContainer}>
+
+        <div className={styles.column}>
+          <div className={styles.item}>
+            <SiHtml5 className={styles.icon} />
+            <p>HTML</p>
+          </div>
+          <div className={styles.item}>
+            <SiCss3 className={styles.icon} />
+            <p>CSS</p>
+          </div>
+          <div className={styles.item}>
+            <SiSass className={styles.icon} />
+            <p>SCSS</p>
+          </div>
+        </div>
+
+        <div className={styles.column}>
+          <div className={styles.item}>
+            <SiJavascript className={styles.icon} />
+            <p>Javascript</p>
+          </div>
+          <div className={styles.item}>
+            <FaReact className={styles.icon} />
+            <p>React</p>
+          </div>
+          <div className={styles.item}>
+            <FaGitAlt className={styles.icon} />
+            <p>Git</p>
+          </div>
+        </div>
+
+        <div className={styles.column}>
+          <div className={styles.item}>
+            <AiFillGithub className={styles.icon} />
+            <p>GitHub</p>
+          </div>
+          <div className={styles.item}>
+            <CgFigma className={styles.icon} />
+            <p>Figma</p>
+          </div>
+          <div className={styles.item}>
+            <SiAdobeillustrator className={styles.icon} />
+            <p>Illustrator</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
