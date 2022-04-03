66f2c1889d9157ac3d74d119a44a6576f3316716
daduckster <vlasak.o@yandex.ru>
2022-04-03 21:53:07 +0200

    final tests

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,42 +11,56 @@ function About() {
       </header>
 
       <div className={styles.textContainer}>
-        <p className={styles.text}>
-        Viverra in interdum elementum aliquam. Amet quis ac malesuada
-        elementum augue sit maecenas tincidunt sit. Id odio eleifend
-        tincidunt vivamus scelerisque. Eget odio in semper sed nunc eget.
-        Turpis aliquet egestas nulla elementum vitae at at vulputate vitae.
-        Arcu vehicula dui, quis purus tincidunt nibh. At vitae, tempus mattis
-        vestibulum suspendisse morbi pulvinar. Ut nam tortor duis quis ac et.
-        </p>
+        <div className={styles.textColumn}>
+          <p className={styles.text}>
+            I am a Frontend JavaScript Developer living in Zurich, Switzerland.
+          </p>
+
+          <p className={styles.text}>
+            I have a degree in an unrelated field, so I had to teach myself programming mostly without mentoring.
+            Throughout all my projects I do not only care for the application's functionality. The design, and the
+            resulting User Experience (UX), are integral parts of my planning process.
+          </p>
+
+          <p className={styles.text}>
+            Since being self-taught comes with certain challenges, I was able to develop life-long skills such as
+            constant learning, problem-solving, and project management.
+          </p>
+
+          <p className={styles.text}>
+            My main technology stack, currently, consists of React and TypeScript on the programming side, as well
+            as Figma and Adobe Illustrator for designs.
+          </p>
+        </div>
 
         <div className={styles.whySection}>
           <h3>WHY ME?</h3>
           <div className={styles.elementContainer}>
             <div className={styles.element}>
               <div className={styles.elementBox}/>
-              <p>Est morbi fermentum fgzfg.</p>
+              <p>High Level of Self-Organization</p>
             </div>
 
             <div className={styles.element}>
               <div className={styles.elementBox}/>
-              <p>Netus aliquet.</p>
+              <p>Experienced Web Designer</p>
             </div>
 
             <div className={styles.element}>
               <div className={styles.elementBox}/>
-              <p>Commodo nisl arcu.</p>
+              <p>Carrying Each Project from Start to Finish</p>
             </div>
 
             <div className={styles.element}>
               <div className={styles.elementBox}/>
-              <p>Est morbi fermentumdfffgx.</p>
+              <p>Constant Improvement of Own Tech Stack</p>
             </div>
 
             <div className={styles.element}>
               <div className={styles.elementBox}/>
-              <p>Diam egestas in amet dfdfg.</p>
+              <p>Strong Communication and Teamwork Skills</p>
             </div>
+
           </div>
         </div>
       </div>
--- src/components/Projects/ProjectsCards.jsx
+++ src/components/Projects/ProjectsCards.jsx
@@ -8,100 +8,96 @@ function ProjectsCards() {
   return (
     <div className={styles.cardsContainer}>
       <article className={styles.card}>
-        <img src={"assets/projectImg.png"} alt="laptop"/>
+        <img src={"assets/doNOTEforget preview.png"} alt="laptop"/>
 
-        <h3>PROJECT NAME1</h3>
+        <h3>Do NOTE Forget</h3>
 
-        <p>Sed commodo nunc dignissim facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque,
-          tempus. Enim aliquet
-          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>...read more</button>
-          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>here's more Sed commodo nunc dignissim
-            facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque, tempus. Sed commodo nunc
-            dignissim facilisis aliquam amet dictum dolor risus.</span>
+        <p className={`${styles.tech}`}>React, TypeScript, SCSS, Figma</p>
+
+        <p className={styles.shortText}>A note keeping app. Aside from functionality and user-experience, a core focus is on style. This meant the inclusion of custom Vector-Graphics (SVG) made in Adobe Illustrator and
+          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>read more</button>
+          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>custom  animations. The user can create new notes and easily navigate through existing notes with the keyword-search feature.</span>
           <button type={'button'} onClick={showLessText} className={`lessBtn ${styles.moreBtn} ${styles.hidden}`}>
             show less</button>
         </p>
 
         <div className={styles.btnContainer}>
           <div className={styles.buttons}>
-            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillPlayCircle
+            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="https://dazzling-kirch-fa75a6.netlify.app/"><AiFillPlayCircle
               className={styles.demoIcon}/> Demo</a>
-            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillGithub
+            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="https://github.com/daduckster/do_note_forget"><AiFillGithub
               className={styles.gitIcon}/> GitHub</a>
           </div>
         </div>
       </article>
 
       <article className={styles.card}>
-        <img src={"assets/projectImg.png"} alt="laptop"/>
+        <img src={"assets/Shop'n'Cook preview.png"} alt="laptop"/>
 
-        <h3>PROJECT NAME2</h3>
+        <h3>Shop'n'Cook</h3>
 
-        <p>Sed commodo nunc dignissim facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque,
-          tempus. Enim aliquet
-          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>...read more</button>
-          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>here's more Sed commodo nunc dignissim
-            facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque, tempus. Sed commodo nunc
-            dignissim facilisis aliquam amet dictum dolor risus.</span>
+        <p className={`${styles.tech}`}>Javascript, SCSS, Figma</p>
+
+        <p className={styles.shortText}>Cookbook and shopping list in one app! Users can store their favourite recipes including the instructions here. If they need to stock up on any of the ingredients, they can mark them for
+          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>read more</button>
+          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>the next trip to the grocery store. Users can also search by dish or specific ingredients.</span>
           <button type={'button'} onClick={showLessText} className={`lessBtn ${styles.moreBtn} ${styles.hidden}`}>
             show less</button>
         </p>
 
         <div className={styles.btnContainer}>
           <div className={styles.buttons}>
-            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillPlayCircle
+            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="https://infallible-wright-becd35.netlify.app/"><AiFillPlayCircle
               className={styles.demoIcon}/> Demo</a>
-            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillGithub
+            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="https://github.com/daduckster/shop-n-cook"><AiFillGithub
               className={styles.gitIcon}/> GitHub</a>
           </div>
         </div>
       </article>
 
       <article className={styles.card}>
-        <img src={"assets/projectImg.png"} alt="laptop"/>
+        <img src={"assets/Bookshelf preview.png"} alt="laptop"/>
 
-        <h3>PROJECT NAME3</h3>
+        <h3>Bookshelf</h3>
 
-        <p>Sed commodo nunc dignissim facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque,
-          tempus. Enim aliquet
-          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>...read more</button>
-          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>here's more Sed commodo nunc dignissim
-            facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque, tempus. Sed commodo nunc
-            dignissim facilisis aliquam amet dictum dolor risus.</span>
+        <p className={`${styles.tech}`}>Javascript, SCSS, Figma</p>
+
+        <p className={styles.shortText}>A new approach to the personal library tracker. Depending on the amount of text supplied to the entry, the books are placed in dynamically shifting columns, ensuring an even
+          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>read more</button>
+          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>spread across the screen. The application allows the user to search their collection using the supplied meta-data (such as author, personal rating, and many more), or filter by reading status.</span>
           <button type={'button'} onClick={showLessText} className={`lessBtn ${styles.moreBtn} ${styles.hidden}`}>
             show less</button>
         </p>
 
         <div className={styles.btnContainer}>
           <div className={styles.buttons}>
-            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillPlayCircle
+            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="https://laughing-galileo-6719bd.netlify.app/"><AiFillPlayCircle
               className={styles.demoIcon}/> Demo</a>
-            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillGithub
+            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="https://github.com/daduckster/bookshelf"><AiFillGithub
               className={styles.gitIcon}/> GitHub</a>
           </div>
         </div>
       </article>
 
       <article className={styles.card}>
-        <img src={"assets/projectImg.png"} alt="laptop"/>
+        <img src={"assets/Restaurant Page preview.png"} alt="laptop"/>
 
-        <h3>PROJECT NAME4</h3>
+        <h3>Restaurant Page</h3>
 
-        <p>Sed commodo nunc dignissim facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque,
-          tempus. Enim aliquet
-          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>...read more</button>
-          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>here's more Sed commodo nunc dignissim
-            facilisis aliquam amet dictum dolor risus. Semper dui venenatis, egestas neque, tempus. Sed commodo nunc
-            dignissim facilisis aliquam amet dictum dolor risus.</span>
+        <p className={`${styles.tech}`}>Javascript, SCSS, Figma</p>
+
+        <p className={styles.shortText}>A whole restaurant page concept the content of which is dynamically generated everytime the users moves between the pages. This effect is achieved through
+          <button type={'button'} onClick={showMoreText} className={`moreBtn ${styles.moreBtn}`}>read more</button>
+          <span className={`moreSpan ${styles.moreSpan} ${styles.hidden}`}>"DOM-Manipulation". The design of the website was made to recreate the website of a fictional modern bistro with a cozy and friendly atmosphere.</span>
           <button type={'button'} onClick={showLessText} className={`lessBtn ${styles.moreBtn} ${styles.hidden}`}>
             show less</button>
         </p>
 
         <div className={styles.btnContainer}>
           <div className={styles.buttons}>
-            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillPlayCircle
+            <a className={styles.demoBtn} target="_blank" rel="noopener noreferrer" href="https://focused-euler-a34b1f.netlify.app/"><AiFillPlayCircle
               className={styles.demoIcon}/> Demo</a>
-            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="#"><AiFillGithub
+            <a className={styles.gitBtn} target="_blank" rel="noopener noreferrer" href="https://github.com/daduckster/restaurant"><AiFillGithub
               className={styles.gitIcon}/> GitHub</a>
           </div>
         </div>
